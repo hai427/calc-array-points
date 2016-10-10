686097ec043da65c6d7c44d5c97795705d0f3316
Hai Nguyen <hai427@uw.edu>
2016-10-10 16:53:56 -0700

    passed all unit tests

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -8,53 +8,83 @@
 
 import Foundation
 
-func add(left : Int, right : Int) -> Int {
-    return left + right
-}
-
-func subtract(left : Int, right : Int) -> Int {
-    return left - right
-}
-
-func multiply(left : Int, right : Int) -> Int {
-    return left * right
-}
-
-func divide(left : Int, right : Int) -> Int {
-    return left / right
-}
-
-func mathOperation(left : Int, right : Int, operation : (Int, Int) -> Int) -> Int {
-    return operation(left, right)
-}
-
-func add(numArray: [Int]) -> Int {
-    var total = 1
-    for num in 0...numArray.count - 1 {
-        total += num
+func add(left : Int?, right : Int?) -> Int? {
+    if left != nil && right != nil {
+        return left! + right!
     }
     
-    return total
+    return nil
 }
 
-func multiply(numArray : [Int]) -> Int {
-    var total = 1
-    for num in 0...numArray.count - 1 {
-        total *= num
+func subtract(left : Int?, right : Int?) -> Int? {
+    if left != nil && right != nil {
+        return left! - right!
     }
-    return total
+    return nil
 }
 
-func count(numArray : [Int]) -> Int {
-    return numArray.count
+func multiply(left : Int?, right : Int?) -> Int? {
+    if left != nil && right != nil {
+        return left! * right!
+    }
+    return nil
 }
 
-func average(numArray : [Int]) -> Int {
-    return add(numArray: numArray) / count(numArray: numArray)
+func divide(left : Int?, right : Int?) -> Int? {
+    if left != nil && right != nil {
+        return left! / right!
+    }
+    return nil
 }
 
-func reduce(numArray : [Int], operation : ([Int]) -> [Int]) -> [Int] {
-    return operation(numArray)
+func mathOperation(left : Int?, right : Int?, operation : (Int?, Int?) -> Int?) -> Int? {
+    if left != nil && right != nil {
+        return operation(left!, right!)
+    }
+    return nil
+}
+
+func add(array: [Int]?) -> Int? {
+    if array != nil {
+        var total = 0
+        for num in 0...((array?.count))! - 1 {
+            total += (array?[num])!
+        }
+        return total
+    }
+    return nil
+}
+
+func multiply(array : [Int]?) -> Int? {
+    if array != nil {
+        var total = 1
+        for num in 0...(array?.count)! - 1 {
+            total *= (array?[num])!
+        }
+        return total
+    }
+    return nil
+}
+
+func count(array : [Int]?) -> Int? {
+    if array != nil {
+        return array?.count
+    }
+    return nil
+}
+
+func average(array : [Int]?) -> Int? {
+    if array != nil {
+        return add(array: array)! / count(array: array)!
+    }
+    return nil
+}
+
+func reduce(array : [Int]?, operation : ([Int]?) -> Int?) -> Int? {
+    if array != nil {
+        return operation(array)
+    }
+    return nil
 }
 
 func add(p1 : (Int, Int), p2 : (Int, Int)) -> (Int, Int) {
@@ -65,30 +95,38 @@ func subtract(p1 : (Int, Int), p2 : (Int, Int)) -> (Int, Int) {
     return ((p1.0 - p2.0), (p1.1 - p2.1))
 }
 
-func add(p1 : [String : Int], p2 : [String : Int]) -> [String : Int] {
+func add(p1 : [String : Int], p2 : [String : Int]) -> [String : Int]? {
     return [
         "x" : p1["x"]! + p2["x"]!,
         "y" : p1["y"]! + p2["y"]!
     ]
 }
 
-func subtract(p1 : [String : Int], p2 : [String : Int]) -> [String : Int] {
+func subtract(p1 : [String : Int], p2 : [String : Int]) -> [String : Int]? {
     return [
         "x" : p1["x"]! - p2["x"]!,
         "y" : p1["y"]! - p2["y"]!
     ]
 }
 
-func add(p1 : [String : Double], p2 : [String : Double]) -> [String : Double] {
-    return [
-        "x" : p1["x"]! + p2["x"]!,
-        "y" : p1["y"]! + p2["y"]!
-    ]
+func add(p1 : [String : Double]?, p2 : [String : Double]?) -> [String : Double]? {
+    if p1 != nil && p2 != nil && p1!["x"] != nil && p1!["y"] != nil && p2!["x"] != nil && p2!["y"] != nil {
+        return [
+            "x" : p1!["x"]! + p2!["x"]!,
+            "y" : p1!["y"]! + p2!["y"]!
+        ]
+    }
+    
+    return nil
 }
 
-func subtract(p1 : [String : Double], p2 : [String : Double]) -> [String : Double] {
-    return [
-        "x" : p1["x"]! - p2["x"]!,
-        "y" : p1["y"]! - p2["y"]!
-    ]
+func subtract(p1 : [String : Double]?, p2 : [String : Double]?) -> [String : Double]? {
+    if p1 != nil && p2 != nil && p1!["x"] != nil && p1!["y"] != nil && p2!["x"] != nil && p2!["y"] != nil {
+        return [
+        "x" : p1!["x"]! - p2!["x"]!,
+        "y" : p1!["y"]! - p2!["y"]!
+        ]
+    }
+    
+    return nil
 }
